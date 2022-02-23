9dcefbb965391ed801a4325e9f4064137a1809de
LanaVl <svit.volkova@gmail.com>
2022-02-23 15:08:55 +0100

    I added my first test

--- cypress/integration/2-advanced-examples/kristall.tests.spec.js
+++ cypress/integration/2-advanced-examples/kristall.tests.spec.js
@@ -1,10 +1,22 @@
 
 /// <reference types = "cypress"/>
 
-describe('The Home Page', () => {
-    
-    it('successfully loads', () => {
-     cy.visit('https://kristallknuten.se/')
+describe('The first test: verify search functionality', () => {
+    it('verify search functionality', () => {  
+        cy.visit("https://kristallknuten.se/")
+        // make sure we are on the home page 
+        cy.contains('Välkommen till Kristallknuten');
+     //get search field with the specific class name
+        cy.get("[class='form-control pl-3 border-0 mobile-search']");
+     
+     it("To check entering in Search field", () => {
+        cy.get('input[type="text"]').type("Halsband").should('have.value', 'Halsband');
+        cy.contains('Search').click() 
+        
     })
-
 })
+
+ 
+ 
+    })
+  
