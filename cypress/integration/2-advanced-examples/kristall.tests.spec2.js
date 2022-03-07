77248c56e2eb12362b5239f9156eee05d47727ed
LanaVl <svit.volkova@gmail.com>
2022-03-07 16:12:16 +0100

    Update kristall.tests.spec2.js

--- cypress/integration/2-advanced-examples/kristall.tests.spec2.js
+++ cypress/integration/2-advanced-examples/kristall.tests.spec2.js
@@ -1,6 +1,6 @@
  
 
- describe('Verify search functionality', () => {
+/* describe('Verify search functionality', () => {
     it('check entering in Search field', () => {  
         cy.visit("https://kristallknuten.se/")
         // make sure we are on the home page 
@@ -10,13 +10,17 @@
         cy.url().should('include', '?s=Halsband');
         
     })
-})  
+})  */
 
 describe('Verify product page functionality', () => {
-    it('check adding item to the cart', () => {  
-        cy.visit("https://kristallknuten.se/shop/search?s=Halsband");
+     it('check adding item to the cart', () => {  
+        cy.visit("https://kristallknuten.se/smycken/halsband/halsband-akvamarin");
         cy.contains('Halsband Akvamarin').click();
-        cy.get('[class = "btn product-btn-addcart btn-block qs-cart-submit qty-cart-submit btn-height-large"]').click();
-      
+        cy.get('[class = "btn product-btn-addcart btn-block qs-cart-submit qty-cart-submit btn-height-large"]').click()
+        cy.url().should('include', 'halsband-akvamarin')
+        
+    //check deliting item from the cart' 
+        cy.get('[class = "qs-cart-delete btn"]').click({force: true});
+        cy.contains('Din varukorg är tom, men det behöver den inte vara.').should('exist')
     })
-})
+    })
